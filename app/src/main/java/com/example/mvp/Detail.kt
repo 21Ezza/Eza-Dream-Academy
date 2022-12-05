465edc396377810fe65cb84d8dbea46bfa981699
21Ezza <zarunda21@gmail.com>
2022-12-05 10:52:49 +0700

    Update Okhttp

---
+++ app/src/main/java/com/example/mvp/Detail.kt
@@ -0,0 +1,35 @@
+package com.example.mvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.lifecycle.MutableLiveData
+import com.bumptech.glide.Glide
+import com.example.mvp.data.model.User
+import com.example.mvp.databinding.ActivityDetailBinding
+
+class Detail : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    private val recipeLiveData = MutableLiveData<List<User>>(listOf())
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val firtName = intent.getStringExtra("firstName")
+        val lastName = intent.getStringExtra("lastName")
+        val email = intent.getStringExtra("email")
+        val avatar = intent.getStringExtra("avatar")
+
+
+        binding.textView1.text = "First Name : $firtName\n" +
+                "Last Name : $lastName\n" +
+                "Email : $email"
+
+        /*val imageView = binding.ivProfile
+        Glide.with(imageView)
+            .load("$avatar")
+            .into(imageView)*/
+
+    }
+}
--- app/src/main/java/com/example/mvp/MainActivity.kt
+++ app/src/main/java/com/example/mvp/MainActivity.kt
@@ -1,14 +1,93 @@
 package com.example.mvp
 
+import android.content.Intent
+import android.hardware.usb.UsbEndpoint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mvp.data.model.User
+import com.example.mvp.data.network.NetworkClient
+import com.example.mvp.data.network.api.ReqresApi
+import com.example.mvpapplication.data.network.ResponseStatus
 import com.example.mvp.databinding.ActivityMainBinding
+import com.example.okhttp.UserAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.*
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val networkClient: NetworkClient = NetworkClient()
+    private val api: ReqresApi = ReqresApi()
+    private val adapter: UserAdapter by lazy { UserAdapter() }
+    private val recipeLiveData = MutableLiveData<List<User>>(listOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rvUser.apply {
+            adapter = this@MainActivity.adapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+
+        recipeLiveData.observe(this) {
+            adapter.submitList(it)
+        }
+
+        binding.btnAsyncCall.setOnClickListener {
+            showProgress(true)
+            api.getUserPagination {
+                when (it) {
+                    is ResponseStatus.SuccessPagination -> {
+                        recipeLiveData.postValue(it.data)
+                    }
+                    is ResponseStatus.Failed -> {
+                        Log.e("MainActivity", it.message)
+                    }
+                    else -> {}
+                }
+                showProgress(false)
+            }
+        }
+
+        binding.btnSyncCall.setOnClickListener {
+            showProgress(true)
+            CoroutineScope(Dispatchers.IO).launch {
+                networkClient.getSync("/recipes").use {
+                    showProgress(false)
+                }
+            }
+        }
+        adapter.setOnCardClickListener(rvListener)
+    }
+
+    private val rvListener: (firstName: String, lastName: String, email: String, avatar: String) -> Unit =
+        { firstName, lastName, email, avatar ->
+            startActivity(Intent(this, Detail::class.java).apply {
+                putExtra("firstName", firstName)
+                putExtra("lastName", lastName)
+                putExtra("email", email)
+                putExtra("avatar", avatar)
+            })
+        }
+
+    private fun showProgress(isShown: Boolean) {
+        CoroutineScope(Dispatchers.Main).launch {
+            binding.progressIndicator.isVisible = isShown
+        }
     }
 }
